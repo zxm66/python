52e11d69d953653a3efce725fb8b0b8c92ebfc7a
zxm66 <1396729865@qq.com>
2020-08-03 23:35:13 +0800

    data class

---
+++ src/data_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+ 
+######################################################################
+# 
+# Copyright (c) 2017 kinva.cc, Inc. All Rights Reserved
+# 
+# @author zhangxiaomin(1396729865@qq.com)
+# 
+######################################################################
+ 
+
+var1 = (1,"hello world")
+print var1
+
+var2 = [1,"hello world"]
+print var2
+
+var3 = {1,"hello world"}
+print var3
+
+
---
+++ src/data_struct.py
@@ -0,0 +1,25 @@
+#!/usr/local/python
+# -*- coding:UTF-8 -*-
+print("hello world")
+
+
+# array
+var1 = [1,2]
+print(var1[1])
+
+# set 
+var2 = {1,2}
+
+print(var2)
+
+for item in var2:
+    print(item)
+
+var3 = {"name":"zhangxiaomin"}
+print(var3)
+
+var3 = {"name":"zhangxiaomin","age":18}
+print(var3)
+
+
+
