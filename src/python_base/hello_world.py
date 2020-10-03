18e3ceceff15f049e0097d9c5abfae37c123938b
zxm66 <1396729865@qq.com>
2020-10-04 04:12:41 +0800

    good night

--- src/python_base/hello_world.py
+++ src/python_base/hello_world.py
@@ -10,18 +10,34 @@
 ######################################################################
  
  
+"""
+    hello zhangxiaomin
+    python基础学习，已经学习完成了。现在我的总结一下了。
+
+"""
+
 __import__('pprint').pprint("this is hello world")
 # 不写mian和写上main方法的区别在于其他模块倒入程序的时候，要执行这段代码
 if __name__ == '__main__':
-    
+    """
+    hello world
+    """
+
     str1 = 'hello world'
+    print(type(str1))
     ch1 = 'c'
+    print(type(ch1))
     number1 = 1
+    print(type(number1))
     number2 = 5555555
+    print(type(number2))
     number3 = 3.2
+    print(type(number3))
     number4 = 3.408000
+    print(type(number4))
     print ("hello world")
     print(id(str1))
     print(hex(id(str1)))
+    print(type.__class__)
 
     pass
