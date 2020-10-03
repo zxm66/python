f4f8a6ee0bfea0409d4c7b3b7b72db972abfaccd
zxm66 <1396729865@qq.com>
2020-10-03 18:17:27 +0800

    function point

--- src/python_base/support_module.py
+++ src/python_base/support_module.py
@@ -7,3 +7,9 @@ world="world"
 def support_print(obj):
     __import__('pprint').pprint("this is support_print method")
     pass
+
+_support_print = support_print
+
+if __name__ == "__main__":
+    _support_print('hello world')
+    pass
