e346af8c5fb82ee683359008b4f5ca860bf863cd
zxm66 <1396729865@qq.com>
2020-10-05 04:48:47 +0800

    vim quickfix list and async run plugin

--- src/python_base/python_attr.py
+++ src/python_base/python_attr.py
@@ -13,11 +13,8 @@ class Person:
     def __str__(self):
         return "name is {},age is {},type is {}".format(self.name,self.age,self.type)
 
-
-
-
 if __name__ == "__main__":
-    print(Person.type)
+
     p = Person("zhangsan", 13)
     print(p)
     # 只是给p加了一个对象属性
--- src/python_base/python_time.py
+++ src/python_base/python_time.py
@@ -1,10 +1,12 @@
 import time
 
 if __name__ == "__main__":
+    print 'world world'
     print(time.time())
     print(time.strftime("%Y-%m-%d %H:%M:%S"))
     print(time.asctime())
-    
+    print 'hello world'   
+    print 'world hello '
     print(time.ctime())
     print(time.daylight)
     print("hello python")
