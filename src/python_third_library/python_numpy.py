241c049b13aacebee2ee3dfe910b33a8a7608aa2
zxm66 <1396729865@qq.com>
2020-10-08 19:22:19 +0800

    python numpy

--- src/python_third_library/python_numpy.py
+++ src/python_third_library/python_numpy.py
@@ -1,6 +1,25 @@
+# 
+# 线性拟合数据
+# 20180815
+
 import numpy as np
-# 可以导入，但是编译器不认识这个东西吗？及时编译
-if __name__ == "__main__":
-    print(np)
-    print("hello world")
-    pass
+#原始数据
+X=[ 1 ,2  ,3 ,4 ,5 ,6]
+Y=[ 2.6 ,3.4 ,4.7 ,5.5 ,6.47 ,7.8]
+
+#用一次多项式拟合，相当于线性拟合
+z1 = np.polyfit(X, Y, 1)
+p1 = np.poly1d(z1)
+print (z1)  #[ 1.          1.49333333]
+print (p1)  # 1 x + 1.493
+
+#作图显示
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(1,7)
+y = z1[0] * x + z1[1]
+plt.figure()
+plt.scatter(X, Y)
+plt.plot(x, y)
+plt.show()
